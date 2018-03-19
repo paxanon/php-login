e50786aec4b2dc1efbddaf8f2cd2e47e61284993
Danny Svane <frodo79@gmail.com>
2018-03-19 15:34:10 +0100

    index

--- index.php
+++ index.php
@@ -1,1 +1,30 @@
-<?php echo "Hello World"; ?>
+<?DOCTYPE-html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<meta http-quiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta name="robots" content="follow">
+
+		<title>Pax Login</title>
+
+		<base href="/" />
+		<!-- UIkit CSS -->
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.40/css/uikit.min.css" />
+	</head>
+
+	<body>
+
+
+	<div .uk-section.uk-container.uk-text-center /div>
+	
+
+
+
+
+		<!-- UIkit JS -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.40/js/uikit.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.40/js/uikit-icons.min.js"></script>
+
+	</body>
+	</html>
