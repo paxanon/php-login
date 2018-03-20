6faff39494f6577998264c58dc8124bb999c8276
Danny Svane <frodo79@gmail.com>
2018-03-20 20:32:03 +0100

    config/footer

---
+++ inc/footer.php
@@ -0,0 +1,5 @@
+footer.php
+		<!-- UIkit JS -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.40/js/uikit.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.40/js/uikit-icons.min.js"></script>
+
--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php require_once "inc/config.php"; ?>
 <?DOCTYPE-html>
 <html lang="en">
 	<head>
@@ -38,11 +39,6 @@
 		</div>
 
 	</div>
-	
-
-
-
-
 		<!-- UIkit JS -->
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.40/js/uikit.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.40/js/uikit-icons.min.js"></script>
